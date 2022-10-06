047dad7a1b0407cf277ae4ac5668bdfd10e93e05
Resty Tronco <resty.tronco@kumu.ph>
2022-10-06 19:45:37 +0800

    QnA model

--- test/set_context_test.py
+++ test/set_context_test.py
@@ -1,4 +1,24 @@
 import requests
+import json
+
+
+
+def get_qa(topic, data):
+    q = []
+    a = []
+    for d in data['data']:
+        if d['title']==topic:
+            for paragraph in d['paragraphs']:
+                for qa in paragraph['qas']:
+                    if not qa['is_impossible']:
+                        q.append(qa['question'])
+                        a.append(qa['answers'][0]['text'])
+            return q,a
+
+with open('train-v2.0.json') as json_file:
+    json_data = json.load(json_file)
+ 
+questions, answers = get_qa(topic='Premier_League', data=json_data  )
 
 json_data = {
   'questions':questions,
@@ -10,4 +30,4 @@ response = requests.post(
   json=json_data
 )
 
-response.json()
+print(response.json())
