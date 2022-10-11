e70f42d17fcc3fe3cdbc2ff9921d55163baa50df
Resty John Tronco <45985429+rjtronco@users.noreply.github.com>
2022-10-11 12:39:47 +0800

    Add files via upload

---
+++ QnA_BentoML/set_context_test.py
@@ -0,0 +1,36 @@
+import requests
+import json
+
+
+
+def get_qa(topic, data):
+    q = []
+    a = []
+    for d in data['data']:
+        if d['title']==topic:
+            for paragraph in d['paragraphs']:
+                for qa in paragraph['qas']:
+                    if not qa['is_impossible']:
+                        q.append(qa['question'])
+                        a.append(qa['answers'][0]['text'])
+            return q,a
+
+with open('train-v2.0.json') as json_file:
+    json_data = json.load(json_file)
+ 
+questions, answers = get_qa(topic='Premier_League', data=json_data  )
+
+json_data = {
+  'flag':'set_context',
+  'data':{
+    'questions':questions,
+    'answers':answers
+  }
+}
+
+response = requests.post(
+  'http://0.0.0.0:3000/predict',
+  json=json_data
+)
+
+print(response.json())
